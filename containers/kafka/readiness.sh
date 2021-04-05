5d863b08121c93080a31750028e4389752b5c10b
Liran Cohen <liran@rct.co.il>
2021-04-05 15:40:45 +0300

    retweak

--- containers/kafka/readiness.sh
+++ containers/kafka/readiness.sh
@@ -2,14 +2,15 @@
 
 /opt/kafka/bin/kafka-topics.sh --create --topic readiness-$HOSTNAME --zookeeper zk-0.zk:2181,zk-1.zk:2181,zk-2.zk:2181 --partitions 1 --replication-factor 2
 
-/opt/kafka/bin/kafka-console-producer.sh --topic readiness-$HOSTNAME --broker-list kafka-0.kafka:9092,kafka-1.kafka:9092,kafka-2.kafka:9092 < echo $HOSTNAME
+/opt/kafka/bin/kafka-topics.sh --zookeeper zk-0.zk:2181,zk-1.zk:2181,zk-2.zk:2181  --alter --topic readiness-$HOSTNAME --config retention.ms=1000
+echo $HOSTNAME|/opt/kafka/bin/kafka-console-producer.sh --topic readiness-$HOSTNAME --broker-list $HOSTNAME.kafka:9092
 
-STAT=$(/opt/kafka/bin/kafka-console-consumer.sh --topic readiness-$HOSTNAME --from-beginning --zookeeper zk-0.zk:2181,zk-1.zk:2181,zk-2.zk:2181
-)
+STAT=$(/opt/kafka/bin/kafka-console-consumer.sh --topic readiness-$HOSTNAME --from-beginning --zookeeper zk-0.zk:2181,zk-1.zk:2181,zk-2.zk:2181 -timeout-ms 2000 )
 
-STAT=$(/opt/zookeeper/bin/zkServer.sh status 2>&1|grep Mode)
+/opt/kafka/bin/kafka-topics.sh --delete --topic readiness-$HOSTNAME  --zookeeper zk-0.zk:2181,zk-1.zk:2181,zk-2.zk:2181 
 
-if [[ "$STAT" =~ "Mode:" ]]; then
+
+if [[ "$STAT" =~ "$HOSTNAME:" ]]; then
     echo "All is well";
     exit 0
 else 
