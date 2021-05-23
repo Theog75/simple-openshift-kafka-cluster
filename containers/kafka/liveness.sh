5419fc20f4722e07bcfe57265eacfbfac487eec0
Liran Cohen <liran@rct.co.il>
2021-05-23 23:29:30 +0300

    d

--- containers/kafka/liveness.sh
+++ containers/kafka/liveness.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/opt/kafka/bin/kafka-topics.sh --create --topic readiness-$HOSTNAME --bootstrap-server kafka-2.kafka:9092,kafka-0.kafka:9092,kafka-1.kafka:9092 --partitions 1 --replication-factor 2
+/opt/kafka/bin/kafka-topics.sh --create --topic readiness-$HOSTNAME --bootstrap-server kafka-2.kafka:9092,kafka-0.kafka:9092,kafka-1.kafka:9092 --partitions 1 --replication-factor 2 &
 
 # /opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka-2.kafka:9092,kafka-0.kafka:9092,kafka-1.kafka:9092  --alter --topic readiness-$HOSTNAME --config retention.ms=1000
 echo $HOSTNAME|/opt/kafka/bin/kafka-console-producer.sh --topic readiness-$HOSTNAME --bootstrap-server kafka-2.kafka:9092,kafka-0.kafka:9092,kafka-1.kafka:9092
