f22b945346c5f0618ada3feb16e4287af1014c37
Liran Cohen <liran@rct.co.il>
2021-05-23 17:52:48 +0300

    d

--- containers/kafka/liveness.sh
+++ containers/kafka/liveness.sh
@@ -1,17 +1,11 @@
-#!/bin/bash
+/opt/kafka/bin/kafka-topics.sh --create --topic readiness-$HOSTNAME --bootstrap-server kafka-2.kafka:9092,kafka-0.kafka:9092,kafka-1.kafka:9092 --partitions 1 --replication-factor 2
+
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka-2.kafka:9092,kafka-0.kafka:9092,kafka-1.kafka:9092  --alter --topic readiness-$HOSTNAME --config retention.ms=1000
+echo $HOSTNAME|/opt/kafka/bin/kafka-console-producer.sh --topic readiness-$HOSTNAME --broker-list $HOSTNAME.kafka:9092
+
+STAT=$(/opt/kafka/bin/kafka-console-consumer.sh --topic readiness-$HOSTNAME --from-beginning --bootstrap-server kafka-2.kafka:9092,kafka-0.kafka:9092,kafka-1.kafka:9092 -timeout-ms 2000 )
+
+/opt/kafka/bin/kafka-topics.sh --delete --topic readiness-$HOSTNAME  --bootstrap-server kafka-2.kafka:9092,kafka-0.kafka:9092,kafka-1.kafka:9092 
 
 
-
-mkdir -p /data/kafka-logs/broker0
-
-ID=$(echo "$HOSTNAME" |sed -r  's/kafka-([0-9])/\1/')
-#MYID=$(expr $ID + 1)
-
-sed  -i "s/broker.id=0/broker.id=$ID/" /opt/kafka/config/server.properties 
-cat /opt/kafka/config/server.properties
-#echo $MYID > /data/myid
-
-echo "!!!!!! Starting Kafka with BrokerID: "$ID
-#/opt/zookeeper/bin/zkServer.sh start-foreground
-/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
-#/opt/kafka/bin/kafka-server-start.sh /tmp/server.properties
+if [[ "$STAT" =~ "$HOSTNAME:" ]]; then
